5870914b97a2e3d0f5b419c91f200452a4c10937
Edvard <heinmetz@chalmers.se>
2024-09-25 22:59:46 +0200

    uuh

--- data.jl
+++ data.jl
@@ -1,13 +1,16 @@
 # Data constants from the project PDF
 
 # Useful constants
-const N_Nodes = 11;
+const N = 11;
 
 # Generator capacities (Unsure if needed)
 capacities = [3271,  2190, 1450, 977, 662, 599, 579, 520, 486, 481, 446, 327, 306, 300];
 
 # All available edges
 const edges = [(1, 2), (1, 11), (2, 3), (2, 11), (3, 4), (3, 9), (4, 5), (5, 6), (5, 8), (6, 7), (7, 8), (7, 9), (8, 9), (9, 10), (10, 11)];
+
+# Adjacency matrix
+const adjacency = [(min(c,r),max(c,r)) in edges ? 1 : 0 for r=1:N, c=1:N ];
 
 # Impedance constants
 b = [-20.1, -22.3, -16.8, -17.2, -11.7, -19.4, -10.8, -12.3, -9.2, -13.9, -8.7, -11.3, -7.7, -13.5, -26.7];
